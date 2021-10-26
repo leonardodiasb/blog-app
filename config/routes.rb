c20113c4d6205ea2dce0c90cd85bc2682e64ce8d
leonardo <leonardodias@ucl.br>
2021-10-26 20:20:20 -0300

    Check routes prefixex

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "/users/:id/posts/:id", to: "posts#show"
-  get "/users/:id/posts", to: "posts#index"
-  get "/users/:id", to: "users#show"
-  get "/users", to: "users#index"
+  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
+  get "/users", to: "users#index", as: "users"
+  get "/users/:id", to: "users#show", as: "user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
