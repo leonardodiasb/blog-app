31f69a91e1168a8ebde88330cb8732729833667e
leonardo <leonardodias@ucl.br>
2021-10-26 20:09:24 -0300

    Generate user posts route, controller and view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/users/:id/posts", to: "posts#index"
   get "/users/:id", to: "users#show"
   get "/users", to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
