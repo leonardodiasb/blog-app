69a8ae65eb0b8e8053e46d12db9aa9fcc6386434
leonardo <leonardodias@ucl.br>
2021-10-28 18:31:20 -0300

    Generate schema file. fix author ref

--- db/migrate/20211028210310_add_author_ref_to_comment.rb
+++ db/migrate/20211028210310_add_author_ref_to_comment.rb
@@ -1,5 +1,5 @@
 class AddAuthorRefToComment < ActiveRecord::Migration[6.1]
   def change
-    add_reference :comments, :user, null: false, foreign_key: true
+    add_reference :posts, :author, null: false, foreign_key: { to_table: :users }
   end
 end
