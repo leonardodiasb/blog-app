df585f5edc0dc46e9368eae7547ba1165ca21b13
leonardo <leonardodias@ucl.br>
2021-10-29 17:18:40 -0300

    Create update comments counter method for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  def update_comments_counter
+    pst = Post.find_by(id: self.id)
+    comment_count = Comment.where(posts_id: self.id).count
+    pst.update(comments_counter: comment_count)
+  end
 end
