07e2fe84dcf1be6683622d94d83cc6bd8e46a3a7
leonardo <leonardodias@ucl.br>
2021-10-29 17:12:46 -0300

    Create update posts counter and return 5 recent comments methods for Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  def update_posts_counter
+    usr = User.find_by(id: self.id)
+    post_count = Post.where(users_id: self.id).count
+    usr.update(posts_counter: post_count)
+  end
+
+  def return_recent_comments
+    Comment.where(posts_id: self.id).order(created_at: :DESC).limit(5)
+  end
 end
