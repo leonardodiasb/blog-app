f69f97967735fad90aa0350664d3be29642cb478
leonardo <leonardodias@ucl.br>
2021-11-04 16:17:17 -0300

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
-  belongs_to :post, dependent: :destroy,  foreign_key: true
-  
+  belongs_to :post, dependent: :destroy, foreign_key: true
+
   def update_likes_counter
     pst = Post.find_by(id: posts_id)
     like_count = Like.where(posts_id: pst.id).count
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
-  
+
   def return_recent_posts
     Post.where(users_id: id).order(created_at: :DESC).limit(3)
   end
