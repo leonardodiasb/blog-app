98744b2a4c9cae8e6fe029b10794bfb7584f41af
leonardo <leonardodias@ucl.br>
2021-11-05 10:45:10 -0300

    Fix migrate relationships, likes-author, comments-author

--- db/migrate/20211028210608_add_author_ref_to_like.rb
+++ db/migrate/20211028210608_add_author_ref_to_like.rb
@@ -1,5 +1,5 @@
 class AddAuthorRefToLike < ActiveRecord::Migration[6.1]
   def change
-    add_reference :posts, :author, null: false, foreign_key: { to_table: :users }
+    add_reference :likes, :author, null: false, foreign_key: { to_table: :users }
   end
 end
