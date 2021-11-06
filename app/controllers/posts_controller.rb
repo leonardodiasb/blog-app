9f6c98712b1eb8aa9fd98b5c774970dba710463e
leonardo <leonardodias@ucl.br>
2021-11-05 21:46:11 -0300

    Add username to comments container

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   def index
+    @users = User.all
     @user = User.find(params[:user_id])
     @posts = @user.return_recent_posts
   end
 
   def show
+    @users = User.all
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comments = @post.return_recent_comments.each
