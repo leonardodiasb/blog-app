e4e934d26611cea0037c4fd1f972e31cf7d8e5b4
leonardo <leonardodias@ucl.br>
2021-11-05 21:54:00 -0300

    Create route, controller and view file for new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,35 @@
 class PostsController < ApplicationController
   def index
+    @users = User.all
     @user = User.find(params[:user_id])
     @posts = @user.return_recent_posts
   end
 
   def show
+    @users = User.all
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comments = @post.return_recent_comments.each
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.text = params[:post][:text]
+    @post.author_id = params[:user_id]
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      @post.update_posts_counter
+      flash[:notice] = 'Post created'
+      redirect_to user_posts_url(@post.author_id)
+    else
+      flash[:error] = 'Post not created'
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do resources :posts, only: [:index, :show] end
+  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+  post "/users/:user_id/posts", to: "posts#create", as: "user_create_post"
+  get "/users/:user_id/posts/new", to: "posts#new", as: "user_new_post"
+  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
+  # post "/users/:user_id/posts/:id", to: "comments#create", as: "user_create_post_comment"
+  # get "/users/:user_id/posts/:id/new", to: "comments#new", as: "user_new_post_comment"
+  # post "/users/:user_id/posts/:id/like", to: "likes#create", as: "user_create_post_like"
+  get "/users", to: "users#index", as: "users"
+  get "/users/:id", to: "users#show", as: "user"
+  root to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
